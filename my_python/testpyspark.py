408046bc7f29b38fc0bc554318388b54a6f3d733
corina9809 <mypost333qweb.de>
2020-11-13 13:32:12 +0100

    First Commit

---
+++ my_python/testpyspark.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession, SQLContext
+from pyspark.sql.types import *
+from pyspark.sql import functions as F
+
+spark = SparkSession.builder.appName('company').getOrCreate()
+sqlContext = SQLContext(spark)
+data = sqlContext.createDataFrame([(1.449015065E9, 1.449015065E9), (0.0,1.449015065E9),(1.449015065E9,0.0),(0.0,0.0)],
+                                  ["modified", "created"])
+data.show()
+data.printSchema()
+
+test_df = data.select(data.modified).show()
+
+test_df = data.select(data.created).show()
